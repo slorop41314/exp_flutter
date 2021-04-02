1dccfbb6520772a2956e930949503912f29346d9
Albert Stanley <albertstanleyy@gmail.com>
2021-04-02 19:51:34 +0700

    feat: basic geolocator

--- lib/features/presentation/screens/main/pick_location.dart
+++ lib/features/presentation/screens/main/pick_location.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PickLocation extends StatefulWidget {
   @override
@@ -9,11 +11,12 @@ class PickLocation extends StatefulWidget {
 }
 
 class _PickLocationState extends State<PickLocation> {
+  double _zoomValue = 15;
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
+  final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    zoom: 15,
   );
 
   static final CameraPosition _kLake = CameraPosition(
@@ -24,29 +27,93 @@ class _PickLocationState extends State<PickLocation> {
   );
 
   @override
+  void initState() {
+    super.initState();
+    this._getCurrentPosition();
+  }
+
+  void _getCurrentPosition() async {
+    final res = await this._determinePosition();
+    _goToLocation(
+      CameraPosition(
+        target: LatLng(res.latitude, res.longitude),
+        zoom: _zoomValue,
+      ),
+    );
+  }
+
+  // Get current position
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    PermissionStatus permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Permission.location.status;
+    if (permission != PermissionStatus.granted) {
+      permission = await Permission.location.request();
+      // permission = await Geolocator.requestPermission();
+      if (permission == PermissionStatus.permanentlyDenied) {
+        print("Permission denied");
+        // Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      if (permission == PermissionStatus.denied) {
+        print("Permission denied");
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future<void> _goToLocation(CameraPosition newPosition) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(newPosition));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
         title: Text("Pick Location"),
       ),
-      body: GoogleMap(
-        mapType: MapType.terrain,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text("Current location"),
+          ),
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.terrain,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                if (!_controller.isCompleted) {
+                  _controller.complete(controller);
+                }
+              },
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
